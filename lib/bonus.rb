5eec31015aeec772c52ec9705a877a1409f74276
boruch d <dubywider@gmail.com>
2019-04-09 18:44:57 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -18,8 +18,8 @@ def bonus
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
-  
+  }
  epic_tragedy[:montague][:hero][:status]= "dead"
-  epic_tragedy[:capulet][:heroine][:status]= "dead  
+epic_tragedy[:capulet][:heroine][:status]= "dead  
 
 end
