50110ecc2c078d1026184db0fe8051d4639cf9a6
boruch d <dubywider@gmail.com>
2019-04-09 18:36:55 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,23 +1,22 @@
 def fourth_challenge
    epic_tragedy = {
-    :montague => {
-      :patriarch => { name: "Lord Montague", age: "53" },
-      :matriarch => { name: "Lady Montague", age: "54" },
-      :hero => { name: "Romeo", age: "15", status: "alive" },
-      :heros_friends => [{ name: "Benvolio", age: "17", attitude: "worried" }, 
-        { name: "Steven", age: "30", attitude: "confused" }
+   :montague => {
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
-        
-      
-    },
-    :capulet => {
-      :patriarch => { name: "Lord Capulet", age: "50" },
-      :matriarch => { name: "Lady Capulet", age: "51" },
-      :heroine => { name: "Juliette", age: "15", status: "alive" },
-      :heroines_friends => [{ name: "Mercutio", age: "18", attitude: "hot-headed" },
-        { name: "Nurse", age: "44", attitude: "worried" }
-    ]
-      }
+   },
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
+      ]
    }
-
+}
 end 
