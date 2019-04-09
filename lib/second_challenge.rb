86cc2eaf89d7b08ca0cf27a7137742a269a332c6
boruch d <dubywider@gmail.com>
2019-04-09 18:09:17 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,18 +1,15 @@
-def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :patriarch =>{},
-     :matriarch =>{},
-     :hero => {},
-     :hero_friends => [],
-      }, 
-    :capulet => {
-      patriarch =>{},
-     :matriarch =>{},
-     :heroine =>{},
-     :heroine_friends =>[]
-      
-   } }
-
-  
-end
+epic_tragedy = {
+    	    :montague => {
+      #your key/value pairs here!	      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => [],
+      }, 	      }, 
+        :capulet => {
+      #your key/value pairs here!	      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+  	      }}
