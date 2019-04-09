862a0508ea5f8d2c4c56eae149fb878317b9e569
boruch d <dubywider@gmail.com>
2019-04-09 18:18:57 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -33,7 +33,7 @@ def third_challenge
         },
       :heroine => {
         #your key/value pairs here
-        name: "Juliette",
+        name: "Juliet",
         age: "15",
         status: "alive"
         },
