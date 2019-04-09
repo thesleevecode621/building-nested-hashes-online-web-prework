b8283387b3b3ccd24ae870d122e1aa6a86949eae
boruch d <dubywider@gmail.com>
2019-04-09 17:44:55 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -34,7 +34,7 @@ def third_challenge
         [:name => "Juliet",
       :age => "15",
       :status => "alive"
-        },
+        }
       :heroine_friends =>[  {
         :name => "Steven",
         :age => "30",
