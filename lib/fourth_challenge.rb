50a127cc69593ab31525a5ec971e478fd51032ed
boruch d <dubywider@gmail.com>
2019-04-09 18:01:18 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,9 +12,9 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [{name:  "Steven", age: "30",attitude: "confused"},
-        {name:  "Nurse", age: "44",attitude: "worried".}
-      ]
+      :heroine_friends => [ {name: "Mercutio", age: "18", attitude: "hot-headed"},
+        {name: "Nurse", age: "44", attitude: "worried"}
+        ]
    }
 }
 
