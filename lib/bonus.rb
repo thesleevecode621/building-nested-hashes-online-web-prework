a6af36f68be5791c0e719b18ec175149c7dfee09
boruch d <dubywider@gmail.com>
2019-04-09 18:41:45 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,8 +19,8 @@ def bonus
       ]
    }
   }
-
-  epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:capulet][:heroine][:status] = "dead"
+end
+  epic_tragedy[:montague][:hero][:status]= "dead"
+  epic_tragedy[:capulet][:heroine][:status]= "dead"
 
 end
